04f21d871ab39a6a5be547b0d0e54e99e3bdf2f8
Михаил <boss@mchebotarev.ru>
2026-07-14 21:15:41 +0300

    add start handler

--- app/bot/handlers/workout.py
+++ app/bot/handlers/workout.py
@@ -1,7 +1,11 @@
 from aiogram import Router, F
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, Message
+from aiogram.filters import Command
 def get_router(workout_service):
     router = Router()
+    @router.message(Command("start"))
+    async def start(message: Message):
+        await message.answer("Бот работает ✅")
     @router.callback_query(F.data.startswith("set_"))
     async def add_set(callback: CallbackQuery):
         workout_id, ex_index = map(int, callback.data.split("_")[1:])
