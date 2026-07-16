9df6c71a65fea63516be01c826e550c1d062b4f7
Михаил <boss@mchebotarev.ru>
2026-07-16 20:09:40 +0300

    fix indentation

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,16 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 class Settings:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    DB_URL = os.getenv("DB_URL")
-    def validate(self):
-        if not self.BOT_TOKEN:
-            raise ValueError("BOT_TOKEN is not set")
-        if not self.DB_URL:
-            raise ValueError("DB_URL is not set")
+BOT_TOKEN = os.getenv(“BOT_TOKEN”)
+DB_URL = os.getenv(“DB_URL”)
+
+def validate(self):
+    if not self.BOT_TOKEN:
+        raise ValueError("BOT_TOKEN is not set")
+    if not self.DB_URL:
+        raise ValueError("DB_URL is not set")
 settings = Settings()
 settings.validate()
