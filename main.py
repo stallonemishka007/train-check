851a95a91abeb5d2411a20498a3d5f943115e509
Михаил <boss@mchebotarev.ru>
2026-07-16 20:13:36 +0300

    fix indentation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 
-from app.core.config import BOT_TOKEN
+from app.core.config import settings
 from app.core.db import create_pool
 
 from app.db.repositories.workout_repo import WorkoutRepository
@@ -10,21 +10,20 @@ from app.services.workout_service import WorkoutService
 from app.bot.handlers.today import get_router as today_router
 from app.bot.handlers.workout import get_router as workout_router
 
-
 async def main():
-    # — бот —
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
+# — бот —
+bot = Bot(token=settings.BOT_TOKEN)
+dp = Dispatcher()
 
-    # --- база ---
-    pool = await create_pool()
-    # --- слои ---
-    repo = WorkoutRepository(pool)
-    service = WorkoutService(repo)
-    # --- роутеры ---
-    dp.include_router(today_router(service))
-    dp.include_router(workout_router(service))
-    # --- запуск ---
-    await dp.start_polling(bot)
-    if __name__ == "__main__":
-        asyncio.run(main())
+# --- база ---
+pool = await create_pool()
+# --- слои ---
+repo = WorkoutRepository(pool)
+service = WorkoutService(repo)
+# --- роутеры ---
+dp.include_router(today_router(service))
+dp.include_router(workout_router(service))
+# --- запуск ---
+await dp.start_polling(bot)
+if name == “main”:
+	asyncio.run(main())
