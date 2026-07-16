bb86b406444486e8112aa2bf9d2b46871c866fe3
Михаил <boss@mchebotarev.ru>
2026-07-16 19:29:58 +0300

    fix indentation

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ from app.bot.handlers.today import get_router as today_router
 from app.bot.handlers.workout import get_router as workout_router
 
 async def main():
-# — бот —
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+    # — бот —
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
 
-# --- база ---
-pool = await create_pool()
-# --- слои ---
-repo = WorkoutRepository(pool)
-service = WorkoutService(repo)
-# --- роутеры ---
-dp.include_router(today_router(service))
-dp.include_router(workout_router(service))
-# --- запуск ---
-await dp.start_polling(bot)
-if name == "name":
-asyncio.run(main())
+    # --- база ---
+    pool = await create_pool()
+    # --- слои ---
+    repo = WorkoutRepository(pool)
+    service = WorkoutService(repo)
+    # --- роутеры ---
+    dp.include_router(today_router(service))
+    dp.include_router(workout_router(service))
+    # --- запуск ---
+    await dp.start_polling(bot)
+    if name == “main”:
+    asyncio.run(main())
