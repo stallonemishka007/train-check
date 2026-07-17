aa66a47b111d1c09e4fafb0b00b409614ac10c67
Михаил <boss@mchebotarev.ru>
2026-07-17 18:10:09 +0300

    fix_config

--- app/bot/handlers/today.py
+++ app/bot/handlers/today.py
@@ -10,14 +10,14 @@ def get_router(service):
 
     @router.message(Command("today"))
     async def today_cmd(message: Message):
-        workout = await service.get_or_create_today()
+        workout = await service.get_today_workout(message.from_user.id)
         await message.answer(
         f"Сегодня\nТренировка {workout['type']}", reply_markup=today_kb(workout["id"])
         )
 
     @router.callback_query(lambda c: c.data == "today")
     async def today_cb(callback: CallbackQuery):
-        workout = await service.get_or_create_today()
+        workout = await service.get_today_workout(message.from_user.id)
         await callback.message.answer(
         f"Сегодня\nТренировка {workout['type']}", reply_markup=today_kb(workout["id"])
         )
