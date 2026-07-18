52d03b15532fe4a8194232fc5c43b3f81124c607
Михаил <boss@mchebotarev.ru>
2026-07-18 12:40:29 +0300

    fix_config

--- app/services/workout_service.py
+++ app/services/workout_service.py
@@ -1,13 +1,11 @@
 from datetime import date
 
-
 class WorkoutService:
     def __init__(self, repo):
         self.repo = repo
 
-async def get_today_workout(self, user_id: int):
-    print("METHOD CALLED ✅")
-    return {"id": 1, "name": "test"}
+    async def get_today_workout(self, user_id: int):
+        return {"id": 1, "name": "test"}
 
 # --- чередование A/B ---
 def get_next_type(self, last_type):
