89ff7e4cadf4a4bb8e852548010679569529657a
Михаил <boss@mchebotarev.ru>
2026-07-21 22:06:38 +0300

    fix_config

--- app/bot/handlers/plan.py
+++ app/bot/handlers/plan.py
@@ -1,23 +1,17 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
-from app.bot.keyboards.inline import plan_kb
 from aiogram.fsm.context import FSMContext
+from app.bot.keyboards.inline import plan_kb
 from app.bot.states.workout import WorkoutState
 def get_router(service):
     router = Router()
     @router.callback_query(lambda c: c.data == "start")
-async def choose_plan(callback: CallbackQuery, state: FSMContext):
-    exists = await service.ensure_user(callback.from_user.id)
-    if not exists:
+    async def choose_plan(callback: CallbackQuery, state: FSMContext):
+        exists = await service.ensure_user(callback.from_user.id)
+        # ✅ ВСЕГДА показываем выбор плана
         await callback.message.edit_text(
             "Выбери план тренировок:",
             reply_markup=plan_kb()
         )
-    else:
-        ex = await service.start_workout(callback.from_user.id)
-        await callback.message.edit_text(
-            f"{ex['name']}\n{ex['done']}/{ex['sets']}\n\nВведи 80x8"
-        )
-        await state.set_state(WorkoutState.waiting_input)
-    await callback.answer()
+        await callback.answer()
     return router
