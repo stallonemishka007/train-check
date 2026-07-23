914995a8fe640f10f26e8014feb12f643fecc08c
Михаил <boss@mchebotarev.ru>
2026-07-23 20:50:54 +0300

    fix_config

--- app/domain/progression.py
+++
@@ -1,4 +0,0 @@
-def get_next_set(last_set, exercise):
-    if last_set:
-        return last_set
-    return exercise["reps"], exercise["weight"]
--- app/domain/workout_logic.py
+++
@@ -1,6 +0,0 @@
-def calculate_status(done, total):
-    if done == 0:
-        return "пропущена"
-    elif done < total:
-        return "частично выполнена"
-    return "выполнена"
